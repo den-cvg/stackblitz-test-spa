3f0636ade0660479a803c9ea2fa3a62e255da3b2
Denis Svirko <denis.svirko@bimeister.com>
2024-06-09 23:24:54 +0300

    feature: AW-9: fix

--- karma.conf.js
+++ karma.conf.js
@@ -7,27 +7,29 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-spec-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      clearContext: false // оставляет браузер открытым
+      clearContext: false, // оставляем false, чтобы контекст не очищался
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/my-app'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'lcovonly' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'lcovonly' }, { type: 'text-summary' }],
     },
-    reporters: ['progress', 'spec', 'coverage'], // Добавляем репортер 'coverage'
+    reporters: ['progress', 'spec', 'coverage'], // используем 'progress', 'spec' и 'coverage'
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'], // Использование безголового режима Chrome
+    browsers: ['ChromeHeadless'], // безголовый режим Chrome
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+    },
   });
 };
