48bc334f3d7ad4c54e173cf0887cb0f9208fd3ce
Denis Svirko <denis.svirko@bimeister.com>
2024-06-09 21:22:13 +0300

    feature: AW-9: fix

--- karma.conf.js
+++ karma.conf.js
@@ -6,18 +6,22 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-spec-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // оставляет браузер открытым
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage/my-app'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+        { type: 'text-summary' }
+      ]
     },
-    reporters: ['progress', 'spec'], // Используем spec репортер
+    reporters: ['progress', 'spec', 'coverage'], // Добавляем репортер 'coverage'
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
